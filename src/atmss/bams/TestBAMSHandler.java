781fc233a3b353dfb50bc2bac15a105ec6836741
FreemanX <12251054@life.hkbu.edu.hk>
2015-11-02 21:57:05 +0800

    debug

--- src/atmss/bams/TestBAMSHandler.java
+++ src/atmss/bams/TestBAMSHandler.java
@@ -1,17 +1,16 @@
 package atmss.bams;
 
-
 // TestBASMHandler
 public class TestBAMSHandler {
-    //------------------------------------------------------------
-    // main
-    public static void main(String[] args) {
-	String urlPrefix = "http://cs6067.comp.hkbu.edu.hk/~group01/";
+	// ------------------------------------------------------------
+	// main
+	public static void main(String[] args) {
+		String urlPrefix = "http://192.168.111.100/";
 
-	// try logging in
-	BAMSHandler bams = new BAMSHandler(urlPrefix);
-	String cred = bams.login("123456789", "456123789");
-	System.out.println("Credential = " + cred);
-	return;
-    } // main
+		// try logging in
+		BAMSHandler bams = new BAMSHandler(urlPrefix);
+		String cred = bams.login("123456789", "987654");
+		System.out.println("Credential = " + cred);
+		return;
+	} // main
 } // TestBAMSHandler
