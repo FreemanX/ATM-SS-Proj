8489f19962342ca2a6fc10b769e82141e1ce4b83
unknown <e4206692@fsc801d-w7-19.csdlabad.local>
2015-11-18 17:11:09 +0800

    Update BAMS handler in MainController. Update Session. doBAMSDeposit is waiting for update, as it only allows int in Joe's code.

--- src/atmss/Session.java
+++ src/atmss/Session.java
@@ -3,17 +3,50 @@
  */
 package atmss;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author freeman
  *
  */
  class Session {
 
+	private long sid = -1;
+	private String cred = "";
+	private String cardNo = "";
+	private List<Operation> ops;
 	/**
 	 * 
 	 */
-	public Session() {
-		// TODO Auto-generated constructor stub
+	public Session(long sid, String cred, String cardNo) {
+		this.sid = sid;
+		this.cred = cred;
+		this.cardNo = cardNo;
+		ops = new ArrayList<Operation>();
 	}
 
+	public void addOp(Operation op) {
+		ops.add(op);
+	}
+
+	public Operation getLastOp() {
+		return ops.get(ops.size() - 1);
+	}
+
+	public List<Operation> getOps() {
+		return ops;
+	}
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public long getSid() {
+		return sid;
+	}
+
+	public String getCred() {
+		return cred;
+	}
 }
