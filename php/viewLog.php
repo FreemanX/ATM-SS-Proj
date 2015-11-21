b1e863aee8a35ec2889c010cc9f3faf24f8db79d
Tony Ting <k999hoshing@gmail.com>
2015-11-21 16:10:03 +0800

    viewLog protect with password, 71552236

--- php/viewLog.php
+++ php/viewLog.php
@@ -4,13 +4,17 @@ require_once("LogHelper.php");
 // _GET lowercase
 $_get_lower = array_change_key_case($_GET, CASE_LOWER);
 
+$hash = "b263b9b19ba7c5c4d8b04a2025c5daca1307e409e814282a4360218a6b2e858b759ccabbacfbd58d0cfcb6eb1ffca843ea5bf9a8f81f3017b2000ba7cea0c228";
+$passwd = $_get_lower["passwd"];
 $pattern = "/".$_get_lower["pattern"]."/";
 $log = new LogHelper();
 $result = "";
 
-foreach ($log->getLogs() as $line) {
-	if (preg_match($pattern, $line)) {
-		$result .= $line."<br>";
+if (strcasecmp($hash, hash("sha512", $passwd, false)) == 0) {
+	foreach ($log->getLogs() as $line) {
+		if (preg_match($pattern, $line)) {
+			$result .= $line."<br>";
+		}
 	}
 }
 
